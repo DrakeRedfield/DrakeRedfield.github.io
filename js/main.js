1a17fbf32fd4c14527cb389119f48112f946fe6c
Angel Antonio Barco Alfaro <drake.redfield@hotmail.com>
2021-06-20 20:00:04 -0500

    Primera versión Landing Page con animacion de scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
+const updatePercentage = () => {
+    //percent.innerHTML = (tl.progress() *100 ).toFixed();
+    timeLine.progress();
+    console.log(timeLine.progress());
+};
+
+let timeLine = new TimelineMax({onUpdate: updatePercentage});
+let timeLine2 = new TimelineMax();
+let timeLine3 = new TimelineMax();
+let timeLineHome = new TimelineMax();
+// let timeLine = new TimelineMax({onUpdate: updatePercentage});
+const ctrlScroll = new ScrollMagic.Controller();
+
+const animationOnScrolling = () => {
+    timeLineHome.from(".cover-text-home", 0.5, {opacity: 1});
+
+    timeLine.from(".cover-text", 0.5, {opacity: 1});
+    timeLine.from(".card-4", 0.5, {y:100,opacity: 0});
+
+    timeLine2.from(".cover-text-hobbies", 0.5, {opacity: 1});
+    timeLine2.from("#about-me-title", 0.5, {x:100,opacity: 0});
+    timeLine2.from(".card-1", .5, {x:-100,opacity: 0});
+    timeLine2.from(".card-2", .5, {y:100,opacity: 0});
+    timeLine2.from(".card-3", .5, {x:100,opacity: 0});
+
+    timeLine3.from(".card-5", .5, {x:100,opacity: 0});
+    timeLine3.from(".card-6", .5, {x:-100,opacity: 0});
+
+    const scene = new ScrollMagic.Scene({
+        triggerElement: ".main-content",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .setPin(".main-content")
+    .setTween(timeLineHome)
+    .addTo(ctrlScroll);
+
+    const scene2 = new ScrollMagic.Scene({
+        triggerElement: ".about-me",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .setPin(".about-me")
+    .setTween(timeLine)
+    .addTo(ctrlScroll);
+    
+    const scene3 = new ScrollMagic.Scene({
+        triggerElement: ".about-me-hobbies",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .setPin(".about-me-hobbies")
+    .setTween(timeLine2)
+    .addTo(ctrlScroll);
+    
+    const scene4 = new ScrollMagic.Scene({
+        triggerElement: ".knowledge",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+    .setPin(".knowledge")
+    .setTween(timeLine3)
+    .addTo(ctrlScroll);
+};
+
 const loadFunction = () => {
     particlesJS('particles-js', {
         particles: {
@@ -45,6 +110,7 @@ const loadFunction = () => {
         /* Retina Display Support */
         retina_detect: true
     });
+    animationOnScrolling();
 }
 
 window.onload = loadFunction;
